d2dc20b0ba75415af52bbd51558c5fa6342ab1d6
wangjunchen <wangjunchen2017@outlook.com>
2022-12-01 17:34:38 +0800

    feat 创建springboot项目seckill-service，添加了redis，mysql，mybatis，seckill-common依赖，主要用于数据的处理

---
+++ seckill-service/src/main/java/com/powernode/seckillservice/SeckillServiceApplication.java
@@ -0,0 +1,13 @@
+package com.powernode.seckillservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillServiceApplication.class, args);
+    }
+
+}
